ebc2792858c953be9c8e9eef81f7f7c285273bce
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-19 20:59:18 -0300

    Problema do INSS 2026

---
+++ decisao/bloco_instrucoes.py
@@ -0,0 +1,4 @@
+print("Hoje está quente mas choveu") 
+print("Se chove, São Paulo fica com trânsito")
+print('E os semáforos normalmente acabam com problemas')
+print('O metrô também fica com velocidade reduzida.')
