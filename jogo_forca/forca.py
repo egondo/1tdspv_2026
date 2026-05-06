4f6c33952f9c1693b68c867790a4b761b9a0f979
Eduardo Gondo <egondo@gmail.com>
2026-05-05 22:47:31 -0300

    Jogo da Forca

--- jogo_forca/forca.py
+++ jogo_forca/forca.py
@@ -1,7 +1,7 @@
 def converte(word: str, letras_chutadas: str) -> str:
     resp = ""
     for c in word:
-        if c in letras_chutadas:
+        if c.lower() in letras_chutadas:
             resp = resp + c + " "
         else:
             resp = resp + "_ "
@@ -10,19 +10,24 @@ def converte(word: str, letras_chutadas: str) -> str:
 
 palavra = "South Caroline"
 err = 0
-chutes = ""
+chutes = " "
 
 segredo = converte(palavra, chutes)
 #enquanto nao enforcou e nao acertou:
 while err < 6 and "_" in segredo: 
     print(segredo)
     print(f"erros: {err}")
-    letra = input("Letra: ")
+    letra = input("Letra: ").lower()
     chutes = chutes + letra
     if not letra in palavra:
         err = err + 1
 
     segredo = converte(palavra, chutes)
+
+if err >= 6:
+    print(f"Você foi enforcado, a palavra é {palavra}")
+else:
+    print(f"Parabéns, a palavra é {palavra} e vc cometeu {err} erros")
 
 #Dar a resposta do jogo, seja quando ele acerta ou perde
 #Resolver a situação em que a palavra possui letras maiúsculas
