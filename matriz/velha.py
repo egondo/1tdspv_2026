1a15359418e080f22242d40c358ba4c8522e6e9f
Eduardo Gondo <egondo@gmail.com>
2026-09-01 19:29:47 -0300

    Jogo da Velha

---
+++ matriz/velha.py
@@ -0,0 +1,18 @@
+def cria_matriz() -> list:
+    mat = []
+    for i in range(3):
+        mat.append([' '] * 3)
+    return mat
+
+
+def tem_espaco(matriz: list) -> bool:
+    return True
+
+def ha_ganhador(matriz: list) -> bool:
+    return False
+
+def joga(matriz: list, lin: int, col: int, jogador: str) -> bool:
+    return True
+
+def imprime(matriz: list):
+    pass
